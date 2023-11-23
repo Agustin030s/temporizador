451b7b4baf6d264ddb928bcb21e6a7f4f5a09286
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-11-22 22:30:52 -0300

    se termino el ejercicio 7

--- js/app.js
+++ js/app.js
@@ -1,54 +1,105 @@
-const inputSegundos = parseInt(document.querySelector('#inputSegundos').value);
 const segundos = document.getElementById('segundos');
 const minutos = document.getElementById('minutos');
-const inputMinutos = document.querySelector('#inputMinutos');
 const horas = document.getElementById('horas');
-const inputHoras = document.querySelector('#inputHoras');
+
+const btnPausar = document.querySelector('#btnPausar');
+const btnReset = document.querySelector('#btnReset');
 
 const formTemporizador = document.getElementById('formTemporizador');
 
 let intervalId;
 
-let contadorSegundos = inputSegundos;
-let contadorMinutos = inputMinutos;
-let contadorHoras = inputHoras;
+let contadorSegundos;
+let contadorMinutos;
+let contadorHoras;
 
-const decrementarSegundos = () =>{
-    segundos.innerText = inputSegundos;
-    if(contadorSegundos >= inputSegundos){
+const decrementarSegundos = () => {
+    document.querySelector('#inputSegundos').value = "";
+    document.querySelector('#inputMinutos').value = "";
+    document.querySelector('#inputHoras').value = "";
+
+    if (contadorSegundos > 0) {
         contadorSegundos--;
         segundos.innerText = contadorSegundos;
-    }else{
-        contadorSegundos = 60;
-        segundos.innerText = 60;
-
-        decrementarMinutos();
+    } else if (contadorMinutos > 0 || contadorHoras > 0) {
+        if (contadorMinutos === 0 && contadorHoras > 0) {
+            contadorMinutos = 60;
+            decrementarHoras();
+        } else if (contadorMinutos > 0) {
+            // contadorMinutos--;
+            contadorSegundos = 60;
+            decrementarMinutos();
+        }
+    } else {
+        clearInterval(intervalId);
     }
 }
 
 const decrementarMinutos = () => {
     contadorMinutos--;
-    
-    if (contadorMinutos >= 0) {
-        minutos.innerText = contadorMinutos + ' : ';
-    } else {
-        contadorMinutos = 60;
-        
-        decrementarHoras();
-    }
+    minutos.innerText = contadorMinutos + ' : ';
 }
 
 const decrementarHoras = () => {
     contadorHoras--;
-    
-    if (contadorHoras >= 0) {
-        horas.innerText = contadorHoras + ' : ';
-    } else {
-        contadorHoras = 0;
+    horas.innerText = contadorHoras + ' : ';
+}
+
+const verificarHoras = (horasInput) =>{
+    if(!isNaN(horasInput)){
+        horas.innerText = horasInput + ' : ';
+    }else{
+        contadorHoras = 0
+        horas.innerText = '00 :';
     }
 }
 
-function mostrar(){
-    console.log(inputSegundos);
+const verificarMinutos = (minutosInput) =>{
+    if(!isNaN(minutosInput)){
+        minutos.innerText = minutosInput + ' : ';
+    }else{
+        contadorMinutos = 0;
+        minutos.innerText = '00 :';
+    }
 }
+
+const verificarSegundos = (segundosInput) =>{
+    if(!isNaN(segundosInput)){
+        segundos.innerText = contadorSegundos;
+    }else{
+        contadorSegundos = 0;
+        segundos.innerText = '00';
+    }
+}
+
+const pausarTemporizador = () =>{
+    clearInterval(intervalId);
+}
+
+const resetTemporizador = () =>{
+    clearInterval(intervalId);
+    contadorSegundos = 0;
+    contadorMinutos = 0;
+    contadorHoras = 0;
+    segundos.innerText = '00';
+    minutos.innerText = '00 : ';
+    horas.innerText = '00 : ';
+}
+
+const iniciarTemporizador = (e) =>{
+    e.preventDefault();
+    contadorSegundos = parseInt(document.querySelector('#inputSegundos').value);
+    contadorMinutos = parseInt(document.querySelector('#inputMinutos').value);
+    contadorHoras =parseInt(document.querySelector('#inputHoras').value);
+
+    verificarHoras(contadorHoras);
+    verificarMinutos(contadorMinutos);
+    verificarSegundos(contadorSegundos);
+    
+    intervalId = setInterval(decrementarSegundos, 1000);
+}
+
+formTemporizador.addEventListener('submit', iniciarTemporizador);
+btnPausar.addEventListener('click', pausarTemporizador);
+btnReset.addEventListener('click', resetTemporizador);
 
